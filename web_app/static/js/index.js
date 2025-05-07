732e52e06d33cba748cbd49a7f86dc66e59f7233
gina_song <11056009@ntub.edu.tw>
2025-05-07 18:46:12 +0800

    編輯-修改了繼承
    (以候用base可以一次繼承nav和footer, 單獨要nav的話就直接繼承nav)

--- web_app/static/js/index.js
+++ web_app/static/js/index.js
@@ -682,11 +682,3 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-
-// 回到最頂層
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth' // 加 smooth 會平滑滾動
-    });
-  }
