2e7a8f2bde77238890d89f83896a1f8058bb7e8e
ikea1229 <11056019@ntub.edu.tw>
2025-05-12 10:11:04 +0800

    修改 - join_detail.html/css/js

--- django_system/urls.py
+++ django_system/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('chat/' , views.chat),
     path('join/' , views.join),
     path('join_create/' , views.join_create),
+    path('join_detail/' , views.join_detail),
     path("test", views.ask_page, name="ask_page"),
     path("ask/", views.ask_view, name="ask_api"),
     path('navbar2/' , views.navbar2),
